dfb9dacac4123e7b5aef5df9fb71f961228e8919
Oriana Photiades <supercandela@gmail.com>
2019-05-27 22:05:52 -0300

    Estudiamos Eventos en JS

--- script.js
+++ script.js
@@ -1,92 +1,49 @@
 
-let nombre = "Candela";
-let apellido = "Bogado";
-let nombreCompleto = nombre + " " + apellido;
+let btn = document.querySelector("button")
 
-let numeros = [10, 7, 8, 0, 4, "Candela"]
-
-numeros.push(35, "Chau");
-
-let persona = {
-    nombre : "Candela",
-    edad : 38,
-    mujer : true,
+/* function hacerClick() {
+    console.log("Hiciste Click!") 
 }
+btn.addEventListener("click", hacerClick) */
 
-persona.cuadro = "Independiente";
+
+ /* Ejercicio
+ Reemplazar el código de la función hacerClick por:
+ 1) Crear un botón por cada click que se haga.
+ 2) Cada botón nuevo se tiene que ver en el DOM.
+ 3) Cada botón nuevo tiene que tener un nuevo callback en su propio evento click también.
+ 4) Todos los botones en pantalla tienen que tener como texto "Botón N", donde N es un número arrancando de 1, el cual se va aumentando de a una unidad. */
 
 
-// Bucle For
-/* for (let i = 0; i < numeros.length; i++) {
-    console.log(numeros[i]);
-    
-} */
 
-//Bucle For In
+ let i = 1
 
-/* for (let prop in persona) {
-    console.log(persona[prop])
-}
+function agregarBoton() {
+    let body = document.querySelector("body")
+    let botonCreado = document.createElement("button") 
+    body.appendChild(botonCreado)
+    botonCreado.innerText = "Botón " + i
+    botonCreado.addEventListener("click", funcionRepetida)
+    i = i + 1     
+ }
+ btn.addEventListener("click", agregarBoton)
+ 
+ function funcionRepetida() {
+     console.log("Le hiciste click a un botón recién creado")
+ }
+ 
+ /*
+En un HTML nuevo:
+1) Hacer una etiqueta button, con un texto que diga "Crear párrafo!".
+2) En JS, agarrar ese nodo.
+3) Registrarle un callback a su click.
+4) El callback del botón tiene que poder crear la siguiente estructura:
+<div>
+ <p>Lorem Ipsum</p>
+ <button>Borrar</button>
+</div>
+
+5) El botón que tiene el texto "Borrar" también tiene que tener un click y su callback tiene que poder borrar el elemento <p>, usando la función
+Nodo.removeChild(Nodo)
+
  */
-
-/* let dos = document.getElementById("item2")
-let items = document.getElementsByClassName("item")
-let lis = document.getElementsByTagName("li") */
-
-/* let dos = document.querySelector('#item2')
-let items = document.querySelector(".item")
-
-console.dir(items) */
-
-let p = document.createElement("p")
-p.innerText = "Mi nuevo texto"
-p.id = "nuevo"
-p.className = "item"
-
-let body = document.querySelector("body")
-
-body.appendChild(p)
-
-//console.log(p)
-
-/*
-1) Crear un elemento <li> y ponerle un texto.
-2) Meterlo como primer ítem del <ul>.
-
-EXTRA
-1) Crear un elemento <ul> nuevo y vacío.
-2) Hacer un bucle de 5 iteraciones.
-3) Por iteración de ciclo, se tiene que crear un <li> nuevo.
-4) Agregar todos los <li> al <ul> del punto 1.
-5) Agregar el <ul> del punto 1 al DOM.
-*/
-
-
-//Ejercicio 1
-let nuevoLi = document.createElement("li")
-nuevoLi.innerText = "Ítem 0 - Agregado"
-nuevoLi.id = "item0"
-nuevoLi.className = "item"
-
-let listaOrig = document.getElementById("ListaOriginal")
-let item1 = document.getElementById("item1")
-listaOrig.insertBefore(nuevoLi,item1)
-
-//EXTRA
-
-//1) Crear un elemento <ul> nuevo y vacío.
-let nuevoUl = document.createElement("ul")
-
-//2) Hacer un bucle de 5 iteraciones.
-for(let i=0; i<5; i++) {
-//3) Por iteración de ciclo, se tiene que crear un <li> nuevo.
-let otroLi = document.createElement("li")
-otroLi.innerText = "Nuevo Texto " + i
-//4) Agregar todos los <li> al <ul> del punto 1.
-nuevoUl.appendChild(otroLi)
-}
-
-//5) Agregar el <ul> del punto 1 al DOM.
-body.appendChild(nuevoUl)
-
-
